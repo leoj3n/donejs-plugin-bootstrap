5ab88b303ea7f36ad8de6c4515e0e313d6b19b2e
Joel Kuzmarski <leoj3n@gmail.com>
2016-06-08 04:58:12 -0500

    :x: core component

--- src/components/core/core.js
+++ src/components/core/core.js
@@ -1,12 +1,15 @@
-import Component from 'can-component';
-import DefineMap from 'can-define/map/';
-import './core.less';
-import template from './core.stache';
+import 'donejs-plugin-bootstrap/css/normalize.css';
+import 'donejs-plugin-bootstrap/css/print.css';
+import 'donejs-plugin-bootstrap/css/reboot.css';
+import 'donejs-plugin-bootstrap/css/type.css';
+import 'donejs-plugin-bootstrap/css/images.css';
+import 'donejs-plugin-bootstrap/css/code.css';
+import 'donejs-plugin-bootstrap/css/grid.css';
+import 'donejs-plugin-bootstrap/css/tables.css';
+import 'donejs-plugin-bootstrap/css/forms.css';
+import 'donejs-plugin-bootstrap/css/buttons.css';
 
-export const ViewModel = DefineMap.extend({});
-
-export default Component.extend({
-  tag: 'bootstrap-core',
-  ViewModel: ViewModel,
-  template
-});
+import 'donejs-plugin-bootstrap/css/utilities.css';
+import 'donejs-plugin-bootstrap/css/utilities-background.css';
+import 'donejs-plugin-bootstrap/css/utilities-spacing.css';
+import 'donejs-plugin-bootstrap/css/utilities-responsive.css';
