9f2ee4484c71c13e32c21cfad3f6fe431407d9b3
Joel Kuzmarski <leoj3n@gmail.com>
2017-03-08 23:45:52 -0600

    Big dump of changes from hack sess

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,20 +3,60 @@ import DefineMap from 'can-define/map/';
 import './navbar.less';
 import template from './navbar.stache';
 
-import "jquery";
-import "donejs-plugin-bootstrap/css/nav.css";
-import "donejs-plugin-bootstrap/css/navbar.css";
-import "donejs-plugin-bootstrap/css/animation.css";
+import $ from "jquery";
+import "~/css/nav.css";
+import "~/css/navbar.css";
+import "~/css/animation.css";
 import "bootstrap/dist/js/umd/collapse";
 
 export const ViewModel = DefineMap.extend({
+  _list: {
+    type: "*",
+    value: []
+  },
   list: {
-    type: "*"
+    type: "*",
+    value: []
+  },
+  initList(items) {
+    this._list = items; // this.attr('list')
+    console.log(this._list);
   }
 });
 
 export default Component.extend({
   tag: 'bootstrap-navbar',
   ViewModel: ViewModel,
-  template
+  template,
+  events: {
+    inserted(el, ev) {
+      console.log(el);
+      this.viewModel.initList(getItems($(el).find('a')));
+    }
+  }
 });
+
+export function getItems(nodeList) {
+  console.log(nodeList);
+  return makeArr(nodeList)
+    .filter(node => node.nodeName === "A")
+    .map(a => getItemFromA(a));
+}
+
+export function getItemFromA(el) {
+  var $el = $(el);
+  return {
+    link: $el.attr('href'),
+    text: $el.text(),
+    active: $el.is(':active')
+  };
+}
+
+/**
+ * Makes array from array-like structure and returns it.
+ * @param arrayLike
+ * @returns {Array.<T>}
+ */
+export function makeArr(arrayLike) {
+  return [].slice.call(arrayLike);
+}
--- test/test.js
+++ test/test.js
@@ -1,2 +1,1 @@
-import '../src/donejs-plugin-bootstrap_test';
 import '../src/components/navbar/navbar_test';
